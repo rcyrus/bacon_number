c532c77e3f9eb762d21bd573bd7429042e6aca51
Rajeev Cyrus <rcyrus@gmail.com>
2014-06-06 20:42:00 -0500

    first  go

---
+++ lib/wikipedia.rb
@@ -0,0 +1,11 @@
+module Wikipedia
+  def self.articles_for(title)
+    client.get_links_for(title)
+  end
+
+  private
+
+  def self.client
+    @client ||= Wikipedia::Client.new
+  end
+end
---
+++ lib/wikipedia/client.rb
@@ -0,0 +1,27 @@
+require 'open-uri'
+require 'json'
+
+module Wikipedia
+  class Client
+    def get_links_for(title)
+      opts = {
+          :action  => "query",
+          :prop    => 'links',
+          :pllimit => '10', #link limit
+          :titles  => title
+      }
+
+      d = JSON::load(URI.parse(url_for(opts)).read)
+      d['query']['pages'].values[0]['links']
+    end
+
+    def url_for(params)
+      url = 'http://en.wikipedia.org/w/api.php?action=:action&format=json'
+      params.inject(url){|furl, kv|
+        value = URI.encode(kv[1]).gsub('&', '%26')
+        furl << "&#{kv[0]}=#{value}"
+      }
+    end
+
+  end
+end
