e1d34e2174b3104513f6360e8ede04663b38a2df
Rajeev Cyrus <rcyrus@gmail.com>
2014-06-12 15:04:50 -0500

    vcr cassets are autoladed.

--- spec/lib/wikipedia_test.rb
+++ spec/lib/wikipedia_test.rb
@@ -5,17 +5,14 @@ describe Wikipedia do
     @client = Wikipedia::Client.new
   }
   it 'return a list of links for topic' do
-    VCR.use_cassette('beer') do
-      x = @client.links_for('Beer')
-      x.must_be_instance_of Array
-    end
+    x = @client.links_for('Beer')
+    x.must_be_instance_of Array
   end
 
   it 'returns nil for an unkown topic' do
     topic = 'derpherptydo'
-    VCR.use_cassette(topic) do
-      @client.links_for(topic).must_be_empty
-    end
+
+    @client.links_for(topic).must_be_empty
   end
 end
 
