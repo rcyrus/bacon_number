1781a0ce54ef78751ed9970cd5836bbb4d08cc6a
Rajeev Cyrus <rcyrus@gmail.com>
2014-06-12 15:03:39 -0500

    automatically load cassets, do not record new ones.
    record new ones if new test cases need them - turn it on manually.

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,4 +1,9 @@
 VCR.configure do |c|
-  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.cassette_library_dir = '../../spec/fixtures/vcr_cassettes'
   c.hook_into :webmock
+
+  c.around_http_request do |request|
+    topic = CGI::parse(request.uri)['titles'].first
+    VCR.use_cassette(topic.gsub(' ', '_'), :record => :none, &request)
+  end
 end
